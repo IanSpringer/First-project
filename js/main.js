1ce9854f4c7d5d7c48382c4617c24f38d37a89cd
Robert Springer <springerdrums@gmail.com>
2016-03-22 10:44:48 -0700

    about to work with phil

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
 
-
-var rulesFunction = function(){
-  $('#rules').remove();
-  $('#play').remove();
-  $('#war-header').append('<p>');
-  $('p').attr('id', 'pTag');
-  $('#pTag').text('War is a classic 2-player card game. When you click "Play" you and the computer will be dealt a hand of 26 cards, half of a standard deck. Once you have your hand, click "Draw" and the first card of your hand and the computer hand will be revealed. The two cards will be compared based on their value. A 2 card has the lowest value and an Ace card has the highest. Whoever possesses the higher valued card wins that round and collects the two cards to be kept in a separate pile. Should two cards with equal value be drawn (ex. King of Hearts and King of Spades), war will be initiated. Click draw again and two more cards will be take from your hand, as well as the computer hand. The player with highest valued card of the six laid out will collect all six cards. Once both hands have been played, the player who collected the most amount of cards wins!');
-  //$('#war-header').append($hTag);
-};
-
 var playFunction = function(){
   $('#rules').remove();
   $('#play').remove();
@@ -23,6 +13,32 @@ var playFunction = function(){
 
 
 
+var rulesFunction = function(){
+  $('#rules').remove();
+  $('#play').remove();
+  $('#war-header').append('<p>');
+  $('p').attr('id', 'pTag');
+  $('#pTag').text('War is a classic 2-player card game. When you click "Play" you and the computer will be dealt a hand of 26 cards, half of a standard deck. Once you have your hand, click "Draw" and the first card of your hand and the computer hand will be revealed. The two cards will be compared based on their value. A 2 card has the lowest value and an Ace card has the highest. Whoever possesses the higher valued card wins that round and collects the two cards to be kept in a separate pile. Should two cards with equal value be drawn (ex. King of Hearts and King of Spades), war will be initiated. Click draw again and two more cards will be take from your hand, as well as the computer hand. The player with highest valued card of the six laid out will collect all six cards. Once both hands have been played, the player who collected the most amount of cards wins!');
+  $('#pTag').append('<div class="playFromRules" id="playFromRules" onclick="playFromRules()">Play</div>')
+};
+
+var playFromRules = function() {
+  $('#rules').remove();
+  $('#play').remove();
+  $('.war').remove();
+  $('p').remove();
+  $('.war-header').append('<div class="draw" onclick="draw()">Draw</div>');
+  $('#reset').append('<div class="resetButton" onclick="reset()">Reset</div>');
+  $('#header2').append('<div class="card1" id="card1"></div>');
+  $('#header2').append('<div class="card2" id="card2"></div>');
+  $('#computerScore').html("Computer Score:");
+  $('#playerScore').html("Player Score:");
+
+};
+
+
+
+
 
 //var Card = function(rank, suit, value) {
   //var cards = {};
@@ -30,9 +46,11 @@ var playFunction = function(){
     //this.suit = suit;
     //this.value = value;
 //};
+
 var cardDeck = {
   suit: ['Hearts', 'Spades', 'Diamonds', 'Clubs'],
   rank: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'],
+
 
 };
 
@@ -93,6 +111,7 @@ var draw = function() {
   computerDraw = computerHand[0];
   playerHand.shift();
   computerHand.shift();
+  // compare cards
   console.log("You drew a " + playerDraw + ", " + "computer drew a " + computerDraw);
 
   return playerHand;
